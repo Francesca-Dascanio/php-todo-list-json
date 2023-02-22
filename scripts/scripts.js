5fa924e80e5032c68ccf077bcaba1217a3a868c5
“Francesca <dascanio.francesca@gmail.com>
2023-02-22 14:38:51 +0100

    Inizializzazione esercizio

---
+++ scripts/scripts.js
@@ -0,0 +1,19 @@
+const { createApp } = Vue;
+
+createApp ({
+    data () {
+        return {
+            // miei dati
+        }
+    },
+    methods: {
+
+    },
+    created () {
+        // axios
+        // .get(url - ridotto)
+        // .then((response) => {
+        //  quali dati deve restituire
+        // });
+    }
+}).mount('#app')
